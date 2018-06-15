978ad24e24d671dd1e9212933ba7a15545fa55ea
Jinhua Wang <sunpotter@msn.cn>
2018-06-15 15:06:28 +0100

    Add two engine files

---
+++ exchange.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+
+/*
+ *  Exchange Smart Contract
+ *  Author: Jinhua Wang 
+ *  Apache License
+ *  Version 2.0, January 2004
+ *  http://www.apache.org/licenses/
+ */
---
+++ underwriteEng.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+/*
+ *  The Option Underwriting Engine on Ethereum
+ *  Author: Jinhua Wang 
+ *  Apache License
+ *  Version 2.0, January 2004
+ *  http://www.apache.org/licenses/
+ */
+
+contract underwriteEng{
+	
+}
